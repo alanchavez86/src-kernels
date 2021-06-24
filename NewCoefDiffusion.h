993250c15231376d9cc6c723d4b2b15947dc4911
alanchavez86 <74686859+alanchavez86@users.noreply.github.com>
2021-06-24 12:14:23 -0600

    Update NewCoefDiffusion.h

--- NewCoefDiffusion.h
+++ NewCoefDiffusion.h
@@ -1,11 +1,6 @@
-//* This file is part of the MOOSE framework
-//* https://www.mooseframework.org
-//*
-//* All rights reserved, see COPYRIGHT for full restrictions
-//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
-//*
-//* Licensed under LGPL 2.1, please see LICENSE for details
-//* https://www.gnu.org/licenses/lgpl-2.1.html
+//* Alan Chavez
+//* Boise State University
+//* 24 June 2021
 
 #pragma once
 
@@ -25,7 +20,5 @@ protected:
   virtual Real computeQpJacobian();
 
 private:
-  const Real _coef;
-  const Function * const _func;
   const MaterialProperty<Real> & _m;
 };
